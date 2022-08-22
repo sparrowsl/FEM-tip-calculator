c036eec9c145be185431352e88e63cfd56a97f76
benjithorpe <benthorpe.dev@gmail.com>
2022-08-22 15:41:39 +0000

    fix output display of values

--- src/lib/components/Output.svelte
+++ src/lib/components/Output.svelte
@@ -7,13 +7,22 @@
     totalBill,
   } from '../store.js';
 
-  // const calculateTip = () => {
+  const resetValues = () => {
+    $tipAmount = 0.0;
+    $totalPerPerson = 0.0;
+    $totalBill = 0.0;
+    $numberOfPeople = 0;
+    $selectedTip = 0;
+  };
+
   $: {
     $tipAmount = Number(
       (($totalBill * ($selectedTip / 100)) / $numberOfPeople).toFixed(2),
     );
   }
-  // };
+  $: {
+    $totalPerPerson = Number(($totalBill / $numberOfPeople).toFixed(2));
+  }
 </script>
 
 <section
@@ -26,7 +35,7 @@
     </p>
     <!-- amount of tip per person -->
     <span class="text-strong_cyan text-2xl font-bold">
-      ${$tipAmount !== Infinity ? $tipAmount : '0.00'}
+      ${$tipAmount && $tipAmount !== Infinity ? $tipAmount : '0.00'}
     </span>
   </div>
 
@@ -36,15 +45,20 @@
       <span class="text-grayish_cyan text-xs">/ person</span>
     </p>
     <!-- amount of total bill for all person -->
-    <span class="text-strong_cyan text-2xl font-bold">${$totalPerPerson}</span>
+    <span class="text-strong_cyan text-2xl font-bold">
+      ${$totalPerPerson && $totalPerPerson !== Infinity
+        ? $totalPerPerson
+        : '0.00'}
+    </span>
   </div>
 
   <!-- reset button -->
   <button
-    disabled
-    type="reset"
-    class="text-very_dark_cyan bg-strong_cyan mt-auto block w-full rounded-md
-      p-2 text-lg font-bold capitalize disabled:cursor-not-allowed"
+    disabled={$tipAmount && $totalPerPerson ? false : true}
+    class="text-very_dark_cyan bg-strong_cyan disabled:text-light_grayish_cyan
+    disabled:bg-dark_grayish_cyan mt-auto block w-full rounded-md p-2 text-lg
+    font-bold capitalize disabled:cursor-not-allowed"
+    on:click={resetValues}
   >
     reset
   </button>
